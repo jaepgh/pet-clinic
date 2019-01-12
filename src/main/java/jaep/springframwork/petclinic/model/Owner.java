27fc7907d25575699e233574d7000d72f71a0de5
jaepgh <jaesquivel2007@gmail.com>
2019-01-11 23:20:31 -0500

    Adding basic POJOs

---
+++ src/main/java/jaep/springframwork/petclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package jaep.springframwork.petclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/jaep/springframwork/petclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package jaep.springframwork.petclinic.model;
+
+public class Vet extends Person {
+}
