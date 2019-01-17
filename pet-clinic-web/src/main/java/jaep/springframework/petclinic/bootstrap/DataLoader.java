77b914628bf9b7d7cc3a84317247cc341704b409
jaepgh <jaesquivel2007@gmail.com>
2019-01-16 23:24:03 -0500

    Listing all owners. Closes #14.

--- pet-clinic-web/src/main/java/jaep/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/jaep/springframework/petclinic/bootstrap/DataLoader.java
@@ -29,21 +29,21 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(1L);
+        owner2.setId(2L);
         owner2.setFirstName("User2");
         owner2.setLastName("Doe2");
 
         ownerService.save(owner2);
 
         Owner owner3 = new Owner();
-        owner3.setId(1L);
+        owner3.setId(3L);
         owner3.setFirstName("User3");
         owner3.setLastName("Doe3");
 
         ownerService.save(owner3);
 
         Owner owner4 = new Owner();
-        owner4.setId(1L);
+        owner4.setId(4L);
         owner4.setFirstName("User4");
         owner4.setLastName("Doe4");
 
