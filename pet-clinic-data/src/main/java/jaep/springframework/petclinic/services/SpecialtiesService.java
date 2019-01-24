d41909ab54569b851a91c49c3386e3908745a816
jaepgh <jaesquivel2007@gmail.com>
2019-01-24 11:47:08 -0500

    Created Specialty service.  Closes #27.

---
+++ pet-clinic-data/src/main/java/jaep/springframework/petclinic/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package jaep.springframework.petclinic.services;
+
+import jaep.springframework.petclinic.model.Specialty;
+
+public interface SpecialtiesService extends CrudService<Specialty, Long> {
+}
--- pet-clinic-data/src/main/java/jaep/springframework/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/jaep/springframework/petclinic/services/VetService.java
@@ -1,7 +1,6 @@
 package jaep.springframework.petclinic.services;
 
 import jaep.springframework.petclinic.model.Vet;
-import org.springframework.stereotype.Service;
 
 public interface VetService extends CrudService<Vet, Long> {
 }
