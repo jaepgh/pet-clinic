b7811f01dd11e78070b32ad1dbd8ed2e4cea114d
jaepgh <jaesquivel2007@gmail.com>
2019-01-24 11:33:44 -0500

    Enhancing owners with contact information and pets.  Closes #29.

--- pet-clinic-web/src/main/java/jaep/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/jaep/springframework/petclinic/bootstrap/DataLoader.java
@@ -1,6 +1,7 @@
 package jaep.springframework.petclinic.bootstrap;
 
 import jaep.springframework.petclinic.model.Owner;
+import jaep.springframework.petclinic.model.Pet;
 import jaep.springframework.petclinic.model.PetType;
 import jaep.springframework.petclinic.model.Vet;
 import jaep.springframework.petclinic.services.OwnerService;
@@ -12,6 +13,8 @@ import jaep.springframework.petclinic.services.map.PetServiceMap;
 import jaep.springframework.petclinic.services.map.VetServiceMap;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -28,29 +31,69 @@ public class DataLoader implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        PetType petType1 = new PetType();
+        petType1.setName("Dog");
+
+        PetType dog = petTypeService.save(petType1);
+
+        PetType petType2 = new PetType();
+        petType2.setName("Cat");
+
+        PetType cat = petTypeService.save(petType2);
+
+        //---------------------------------------------------
+
+
+        //---------------------------------------------------
+
         Owner owner1 = new Owner();
         owner1.setFirstName("User1");
         owner1.setLastName("Doe1");
+        owner1.setAddress("123 SW 25th Ct");
+        owner1.setCity("Miami");
+        owner1.setTelephone("3054562222");
+
+        Pet pet1 = new Pet();
+        pet1.setPetType(dog);
+        pet1.setBirthDay(LocalDate.now());
+        pet1.setName("Lucky");
+        pet1.setOwner(owner1);
+        owner1.getPets().add(pet1);
 
         ownerService.save(owner1);
+
 
         Owner owner2 = new Owner();
         owner2.setFirstName("User2");
         owner2.setLastName("Doe2");
+        owner2.setAddress("456 NW 14t St");
+        owner2.setCity("North Miami");
+        owner2.setTelephone("7864457898");
+
+        Pet pet2 = new Pet();
+        pet2.setPetType(cat);
+        pet2.setBirthDay(LocalDate.now().minusYears(1));
+        pet2.setName("Kitty");
+        pet2.setOwner(owner2);
+        owner2.getPets().add(pet2);
 
         ownerService.save(owner2);
 
         Owner owner3 = new Owner();
         owner3.setFirstName("User3");
         owner3.setLastName("Doe3");
+        owner3.setAddress("789 Pines Blvd");
+        owner3.setCity("Pembroke Pines");
+        owner3.setTelephone("9544568787");
+
+        Pet pet3 = new Pet();
+        pet3.setPetType(dog);
+        pet3.setBirthDay(LocalDate.now().minusMonths(3));
+        pet3.setName("Boby");
+        pet3.setOwner(owner3);
+        owner3.getPets().add(pet3);
 
         ownerService.save(owner3);
-
-        Owner owner4 = new Owner();
-        owner4.setFirstName("User4");
-        owner4.setLastName("Doe4");
-
-        ownerService.save(owner4);
 
         //---------------------------------------------------
 
@@ -74,14 +117,5 @@ public class DataLoader implements CommandLineRunner {
 
         //---------------------------------------------------
 
-        PetType petType1 = new PetType();
-        petType1.setName("Dog");
-
-        PetType dog = petTypeService.save(petType1);
-
-        PetType petType2 = new PetType();
-        petType2.setName("Cat");
-
-        PetType cat = petTypeService.save(petType2);
     }
 }
