803657e8d63fec3fdb545c128c3efbd4f5ae1da6
jaepgh <jaesquivel2007@gmail.com>
2019-01-27 23:29:28 -0500

    Converted Owners to JPA entities. Closes #32.

--- pet-clinic-data/src/main/java/jaep/springframework/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/jaep/springframework/petclinic/model/Pet.java
@@ -1,13 +1,24 @@
 package jaep.springframework.petclinic.model;
 
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pets")
 public class Pet extends BaseEntity {
+
+    @ManyToOne
+    @JoinColumn(name = "type_id")
     private PetType petType;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
     private Owner owner;
+    @Column(name = "birth_day")
     private LocalDate birthDay;
+    @Column(name = "name")
     private String name;
+
 
     public PetType getPetType() {
         return petType;
