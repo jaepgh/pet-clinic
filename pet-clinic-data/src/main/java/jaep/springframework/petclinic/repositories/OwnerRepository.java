4353924baa885bf43a8c5d6740af5a1d2fa6030e
jaepgh <jaesquivel2007@gmail.com>
2019-01-31 22:08:07 -0500

    Spring Data JPA for Owners created. Closes #36.

--- pet-clinic-data/src/main/java/jaep/springframework/petclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/jaep/springframework/petclinic/repositories/OwnerRepository.java
@@ -3,8 +3,7 @@ package jaep.springframework.petclinic.repositories;
 import jaep.springframework.petclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Optional;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
-
+    Owner findByLastName(String lastName);
 }
