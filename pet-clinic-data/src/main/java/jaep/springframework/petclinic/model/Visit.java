2af587d9b3f1944a584299cbc5dcfe763763ae2f
jaepgh <jaesquivel2007@gmail.com>
2019-02-22 00:27:56 -0500

    Vets view refactored to show the specialty. Closes #70.

--- pet-clinic-data/src/main/java/jaep/springframework/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/jaep/springframework/petclinic/model/Visit.java
@@ -1,6 +1,7 @@
 package jaep.springframework.petclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
